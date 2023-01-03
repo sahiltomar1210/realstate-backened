f90452c7eb2fcffd1b90abe2aa1a2f02f0ea1caf
Sahil Tomar <113227437+sahiltomar1210@users.noreply.github.com>
2023-01-03 21:07:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(routes);
 
 const secret ="RESTAPI"
 //Router MIddlewares
